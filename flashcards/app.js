86845ca05e50fad26a9a29d02790f809ab39c2a1
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-05 21:36:30 -0700

    set up express server

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,13 +1,67 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+const friends = [
+    "lady gabor",
+    'beatles',
+    'queen the mane',
+    'sir lancelot'
+];
+
+
+const friendsList = [
+    {
+        "first": "Tupac",
+        "last": "Shakur"
+    },
+    {
+        "first": "Snoop",
+        "last": "Dogg"
+    },
+    {
+        "first": "T",
+        "last": "Payne"
+    },
+    {
+        "first": "Missy",
+        "last": "Elliot"
+    },
+    {
+        "first": "Doja",
+        "last": "Cat"
+    },
+];
+
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send(`<h1>I love testing</h1>`);
+    res.render(`index.pug`);
 });
 
-app.get('/about', (req, res) => {
-    res.send(`<h1>Sending hellos!</h1>`);
+app.get('/cards', (req, res) => {
+    res.render(`cards`, {prompt: `Who is buried in Grant's tomb?`});
+});
+
+app.get('/hello', (req, res) => {
+    res.render(`hello`, {});
+});
+
+app.post('/hello', (req, res) => {
+    res.json(req.body);
+});
+
+
+
+
+//   /sandbox
+// First Name | Last Name
+app.get('/sandbox', (req, res) => {
+    res.render(`sandbox`, {prompt: `Who is buried in Grant's tomb?`, friendsList});
 });
 
 app.listen(3000, () => {
