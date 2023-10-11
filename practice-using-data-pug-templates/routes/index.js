613f471a252e06370be56ec099960ad4dfeff07a
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-11 16:16:50 -0700

    finished practice using data pug templates

--- practice-using-data-pug-templates/routes/index.js
+++ practice-using-data-pug-templates/routes/index.js
@@ -5,17 +5,18 @@ const { recipes } = require('../data/data.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // 1. Pass all recipe data to 'index' template
-  res.render('index');
+  res.render('index', { recipes });
 });
 
 /* GET recipe page. */
 router.get('/recipes/:id', function(req, res, next) {
   const recipeId = req.params.id;
   const recipe = recipes.find( ({ id }) => id === +recipeId );
+  console.dir(recipe)
   
   if (recipe) {
     // 2. Pass the recipe data to the 'recipe' template
-    res.render('recipe');
+    res.render('recipe', { recipe });
   } else {
     res.sendStatus(404);
   }
