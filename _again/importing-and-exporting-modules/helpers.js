2e3431c6c4a59455b36cf5b77160a21c88de34f0
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-10-12 16:28:38 -0700

    practice with data pug templates again

---
+++ _again/importing-and-exporting-modules/helpers.js
@@ -0,0 +1,9 @@
+// Helper function to reverse a string
+const reverseString = (string) => [...string].reverse().join('');
+
+// Helper function to shorten a string to fifty characters
+const shortenString = (string) => {
+  return string.length > 50 ? string.substring(0, 50) + "..." : string;
+}
+
+module.exports = { reverseString, shortenString };
---
+++ _again/practice-using-data-pug-templates/README.md
@@ -0,0 +1,11 @@
+## FSJS practice session project
+
+### Practice Using Data with Pug Templates
+
+#### Practice instructions:
+
+* **1) Setup app**
+  * Navigate to the project directory
+  * Run `npm install` to install dependencies
+  * Run `npm start` to launch the app
+  
